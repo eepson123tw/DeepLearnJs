d6e6bd3e9674e80a7c1c4543c94f1ceb7df16989
allen <eepson123@gmail.com>
2022-06-15 21:45:46 +0800

    add DeviceOrientationEvent

---
+++ 06 dom/EventApis/Event/DeviceOrientationEvent.js
@@ -0,0 +1,12 @@
+/* DeviceOrientationEvent  提供给网页开发者当设备（指手机，平板等移动设备）在浏览页面时物理旋转的信息。
+
+DeviceOrientationEvent.absolute - 只读用来说明设备是提供的旋转数据是否是绝对定位的布尔值。
+DeviceOrientationEvent.alpha - 只读一个表示设备绕 z 轴旋转的角度（范围在 0-360 之间）的数字
+ 
+DeviceOrientationEvent.beta - 只读一个表示设备绕 x 轴旋转（范围在－180 到 180 之间）的数字，从前到后的方向为正方向。
+DeviceOrientationEvent.gamma - 只读一个表示设备绕 y 轴旋转（范围在－90 到 90 之间）的数字，从左向右为正方向。
+
+
+*/
+
+console.dir(DeviceOrientationEvent);
