e6c870ed7831282a358fa8ef7811f7b5a7e8a354
allen <allenshih@starlux-airlines.com>
2022-07-18 12:08:11 +0800

    feat:add the fill api done

--- 10資料結構操作及實現/arrayApi.js
+++ 10資料結構操作及實現/arrayApi.js
@@ -297,7 +297,7 @@ var newArray = arr.filter(callback(element[, index[, array]])[, thisArg])
 ---
 
 mutable
-*Array.prototype.fill()
+*Array.prototype.fill() Done
 fill() 方法用一个固定值填充一个数组中从起始索引到终止索引内的全部元素。不包括终止索引。
 arr.fill(value[, start[, end]])
 
