da7f10a16625e9d65a24607db2867b5af20b804f
Allen Shih 施周杰 <2203345@C0962.local>
2022-08-02 16:40:32 +0800

    feat: add the some Api

--- 10資料結構操作及實現/arrayApi.js
+++ 10資料結構操作及實現/arrayApi.js
@@ -87,7 +87,7 @@ shift() 方法从数组中删除第一个元素，并返回该元素的值。此
 ---
 
 mutable
-*Array.prototype.reverse()
+*Array.prototype.reverse() Done
 reverse() 方法将数组中元素的位置颠倒，并返回该数组。
 数组的第一个元素会变成最后一个，数组的最后一个元素变成第一个。该方法会改变原数组。
 
@@ -558,13 +558,20 @@ const coDataAry = (ary) => {
         }
       }
       return res
+    },
+    some(fn) {
+      let res = false
+      for (let item of ary) {
+        res = fn(item)
+      }
+      return res
     }
   }
 }
 
 let ary = coDataAry([1, [2, 2, 3, [1, 3, [6]]]])
 let a = ary.flat()
-
+// console.log(a.some((x) => x > 2))
 // a.concat(1, 2, 3).log()
 // console.log(a.indexOf(1))
 // a.map((x) => ({
