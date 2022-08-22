e66e295a0f0ea7d819d8676140a9f14d66150bc2
allen <eepson123@gmail.com>
2022-08-22 23:01:02 +0800

    feat:Add the simple debounce

--- 10資料結構操作及實現/lodash&uncderCore.js
+++ 10資料結構操作及實現/lodash&uncderCore.js
@@ -30,20 +30,17 @@ const throttleFn = (fn, wait = 0, options) => {
   }
 }
 
-// const a = throttleFn(() => {
-//   console.log('aap')
-// }, 1000)
+const debounce = (fn, sec) => {
+  let timeFn
+  if (timeFn) {
+    clearTimeout(timeFn)
+  }
+  timeFn = () => setTimeout(() => fn, sec)
+  return timeFn
+}
 
-// setTimeout(() => {
-//   a()
-// }, 2000)
+let app = debounce(console.log(123), 1000)
 
-// setTimeout(() => {
-//   a()
-// }, 2500)
-// setTimeout(() => {
-//   a()
-// }, 3000)
 /* 
 
 
@@ -51,10 +48,11 @@ const throttleFn = (fn, wait = 0, options) => {
 
 
 debounce_.debounce(function, wait, [immediate])
-Creates and returns a new debounced version of the passed function which will postpone its execution until after wait milliseconds have elapsed since the last time it was invoked. Useful for implementing behavior that should only happen after the input has stopped arriving. For example: rendering a preview of a Markdown comment, recalculating a layout after the window has stopped being resized, and so on.
-
+Creates and returns a new debounced version of the passed function which will postpone its execution until after wait milliseconds
+have elapsed since the last time it was invoked. 
+Useful for implementing behavior that should only happen after the input has stopped arriving.
+For example: rendering a preview of a Markdown comment, recalculating a layout after the window has stopped being resized, and so on.
 At the end of the wait interval, the function will be called with the arguments that were passed most recently to the debounced function.
-
 Pass true for the immediate argument to cause debounce to trigger the function on the leading instead of the trailing edge of the wait interval. Useful in circumstances like preventing accidental double-clicks on a "submit" button from firing a second time.
 
 var lazyLayout = _.debounce(calculateLayout, 300);
